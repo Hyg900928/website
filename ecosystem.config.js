60c531adc22d4e9e507eee3ab689083506128a99
贺运高 <1040083796@qq.com>
2018-11-14 12:44:57 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
         repo: "git@github.com:Hyg900928/website.git",
         path: "/root/workspace/www/website",
         ssh_options: "StrictHostKeyChecking=no",
-        "post-deploy": 'cnpm install && pm2 startOrRestart ./ecosystem.config.js --env production',
+        "post-deploy": 'cnpm install && npm run server --env production',
         env: {                                               
           NODE_ENV: 'production'
         }
