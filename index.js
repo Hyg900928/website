67c0980358c7e9824a7007e55bcf7c38368e1184
贺运高 <1040083796@qq.com>
2018-11-14 12:29:07 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const exress = require('express')
+
+const app = exress()
+
+app.get('/', function(req, res, next){
+    res.end('hello world \n')
+    next()
+})
+
+app.listen(3000, function() {
+    console.log('server is running at port 3000')
+})
